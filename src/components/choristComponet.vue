598638c2b397f3de86a400cec8f34717f6b017bd
Ber30 <makoberobertrand@gmail.com>
2022-12-27 20:53:50 +0200

    fixing some eslint warnings

--- src/components/choristComponet.vue
+++ src/components/choristComponet.vue
@@ -2,13 +2,13 @@
   <ion-item-sliding>
     <ion-item-options side="start">
       <ion-item-option color="success">
-        <ion-icon slot="bottom" :icon="archive"></ion-icon>
+        <ion-icon></ion-icon>
         Present
       </ion-item-option>
     </ion-item-options>
 
     <ion-item>
-        <ion-avatar slot="start">
+        <ion-avatar>
         <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
       </ion-avatar>
       <ion-label>
@@ -18,7 +18,7 @@
 
     <ion-item-options>
       <ion-item-option color="danger">
-        <ion-icon slot="bottom" :icon="trash"></ion-icon>
+        <ion-icon></ion-icon>
         Absent
       </ion-item-option>
     </ion-item-options>
